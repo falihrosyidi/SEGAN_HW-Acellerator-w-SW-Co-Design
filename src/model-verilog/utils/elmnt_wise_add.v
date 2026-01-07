d214788540218bf94b1aa66209829e4c9d5409db
n0thing010 <testes6910@gmail.com>
2026-01-07 15:32:38 +0700

    add:element wise add for total multiplication

---
+++ src/model-verilog/utils/elmnt_wise_add.v
@@ -0,0 +1,25 @@
+`ifndef ELMT_WISE_ADD
+`define ELMT_WISE_ADD
+
+
+module elmt_wise_add #(
+    parameter WIDTH = 32,
+    parameter FBITS = 24,
+    parameter N_REG = 31
+) (
+    input wire signed  [N_REG*WIDTH-1:0] out_multiply,
+    output reg signed [WIDTH-1:0] y
+);
+    integer i;
+    reg signed [WIDTH-1:0] temp_sum;
+
+    always @* begin
+        temp_sum = 0; 
+        for (i = 0; i < N_REG; i = i + 1) begin
+            temp_sum = temp_sum + out_multiply[i*WIDTH +: WIDTH];
+        end
+        y = temp_sum;
+    end
+
+endmodule
+`endif
